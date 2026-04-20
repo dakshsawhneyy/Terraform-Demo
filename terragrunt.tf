97e0cb998d29a9d0024b71ed0f1912b92081bce0
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2026-04-20 18:47:44 +0530

    Update terragrunt.tf

--- terragrunt.tf
+++ terragrunt.tf
@@ -46,3 +46,18 @@ provider "aws" {
 }
 EOF
 }
+
+
+# Remote Backend
+remote_state {
+  backend = "s3"
+  generate = {
+    path      = "backend.tf"
+    if_exists = "overwrite_terragrunt"
+  }
+  config = {
+    bucket         = "my-unique-terraform-state-bucket"
+    key            = "${path_relative_to_include()}/terraform.tfstate"
+    region         = "us-east-1"
+  }
+}
