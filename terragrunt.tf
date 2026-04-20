2cd66431c7e934aa80f547605c4752feaec84eae
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2026-04-20 18:42:57 +0530

    Create terragrunt.tf

---
+++ terragrunt.tf
@@ -0,0 +1,30 @@
+# Infrastructure Modules
+
+infrastrcture-modules/vpc/
+
+resource "aws_vpc" "demo" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    env = var.env
+  }
+}
+
+# Subnets for dependency
+resource "aws_subnet" "demo" {
+  vpc_id = aws_vpc.demo.id
+  cidr_block = var.subnet_cidr
+  tags = {
+    env = var.env
+  }
+}
+
+# Output this vpc id and subnet id for ec2 creation
+output "vpc_id" {
+  value = aws_vpc.demo.id
+}
+
+output "subnet_id" {
+  value = aws_subnet.demo.id
+}
+
+
